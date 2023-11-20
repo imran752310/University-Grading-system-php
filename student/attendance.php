e48ca3df9eda6ece19865127eaebc31fbb745738
Muhammad imran khan <81646567+imran752310@users.noreply.github.com>
2023-11-20 22:14:30 +0500

    Update attendance.php

--- student/attendance.php
+++ student/attendance.php
@@ -17,8 +17,8 @@ $courses_res = mysqli_query($conn, $courses_query);
 ?>
 
 <div class="container-fluid">
-    <div class="card shadow mb-4" style="border-radius: 40px;">
-        <div class="card-header py-3" style="border-radius: 40px;">
+    <div class="card shadow mb-4" >
+        <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Attendance</h6>
         </div>
         <div class="card-body">
